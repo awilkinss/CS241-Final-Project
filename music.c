2f8b9c531d7fa96f1ad42950bcb95a5960b41d89
Jesse Cruz <jcruz2@oberlin.edu>
2022-05-31 11:40:00 -0400

    all functions added, seg fault :(

--- music.c
+++ music.c
@@ -11,6 +11,7 @@ struct node{
   struct node *prev;
 };
 struct list{
+  int size;
   struct node *head;
   struct node *tail;
 };
@@ -45,6 +46,7 @@ struct list *createList(){
     perror("Error!");
     exit(1);
   }
+  newList->size = 0;
   newList->head = head;
   newList->tail = tail;
   newList->head->next = newList->tail;
@@ -107,12 +109,14 @@ void insertNode(struct node *chord, struct list *chords){
     head->next = chord;
     temp->prev = chord;
     chord->prev = head;
+    chords->size = chords->size + 1;
   }
   else{
     head->next = chord;
     chord->next = tail;
     tail->prev = chord;
     chord->prev = head;
+    chords->size = chords->size + 1;
   }
 }
 void insertDictionary(struct dictionary *newDict, struct dictList *song){
@@ -155,11 +159,84 @@ void updateDictionaryList(char *dictionaryKey,char *chord, struct dictList *song
     updateDictionary(chord,dict);
   }
 }
-    
+
+//chord picker related stuff
+int * frequencyArrayMaker(char *c, struct dictList *dictListSong){
+  struct dictionary *chord = fetchDictionary(c,dictListSong);
+  struct list *chords = chord->chords;
+  struct node *temp = chords->head->next;
+  int n = chords->size;
+  int array[n];
+  int i = 0;
+  while(temp != chords->tail){
+    array[i] = temp->freq;
+    temp = temp->next;
+    i++;
+  }
+  return array;
+}
+
+char ** chordArrayMaker(char *c, struct dictList *song){
+  struct dictionary *chord = fetchDictionary(c,song);
+  struct list *chords = chord->chords;
+  struct node *temp = chords->head->next;
+  char **array = NULL;
+  array = malloc(sizeof(char *) * chords->size);
+  for(int k = 0; k < chords->size; k++){
+    array[k] = malloc((chords->size+1)*sizeof(char));
+  }
+  int i = 0;
+  while(temp != chords->tail){
+    strcpy(array[i], temp->key);
+    temp = temp->next;
+    i++;
+  }
+  return array;
+}
+
+int findCeil(int arr[], int r, int l, int h){
+  int mid;
+  while(l < h){
+    mid = l + ((h-l) >> 1);
+    (r > arr[mid]) ? (l = mid +1) : (h = mid);
+  }
+  return (arr[l] >= r) ? l : -1;
+}
+
+char * chooseChord(char **chords,int *frequency, int n){
+  int freq[n];
+  for(int m =0; m < n; m++){
+    freq[m] = *(frequency+m);
+  }
+  int prefix[n], i;
+  prefix[0] = freq[0];
+  for (i = 1; i < n; ++i)
+    prefix[i] = prefix[i - 1] + freq[i]; 
+  int r = (rand() % prefix[n - 1]) + 1;
+  int indexc = findCeil(prefix, r, 0, n - 1);
+  return chords[indexc];
+}
+
 int main(){
 
   struct dictList *song = createDictList();
-  printf("%s\n",song->head->key);
+  char newChord[24][10];
+  char chosen[10] = "C";
+  updateDictionaryList("C","F",song);
+  updateDictionaryList("F","D",song);
+  updateDictionaryList("D","C",song);
+  updateDictionaryList("C","B",song);
+  updateDictionaryList("B","C",song);
+  updateDictionaryList("C","G",song);
+  struct dictionary *dict = fetchDictionary(chosen,song);
+  int n = dict->chords->size;
+  printf("%d",n);
+  for(int j = 1; j < 24; j++){  
+    strcpy(chosen,chooseChord(chordArrayMaker(chosen,song),frequencyArrayMaker(chosen,song),n));
+    strcpy(newChord[j], chosen);
+    printf("%s",newChord[j]);
+  }
+  /*printf("%s\n",song->head->key);
   printf("%s\n",song->head->chords->head->key);
   updateDictionaryList("Cmaj","Fmin", song);
   printf("%s\n",song->head->next->key);
@@ -170,7 +247,8 @@ int main(){
   printf("This should be s:%s\n",song->head->next->next->key);
   printf("This should be Fmin:%s\n",song->head->next->chords->head->next->key);
   printf("This shoudl be s:%s\n",song->head->next->chords->head->next->next->key);
-  printf("This should be Fmin Freq 2:%d\n",song->head->next->chords->head->next->freq);
+  printf("This should be Fmin Freq 2:%d\n",song->head->next->chords->head->next->freq);*/
+  
 
   return 1;
   
